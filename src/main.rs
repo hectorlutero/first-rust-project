d79c84bca8301cd15ff35433dc33193c41b48abd
Hector Siman <hectorlutero@hotmail.com>
2023-02-05 00:45:30 -0300

    [FEATURE] updating cli-calculator to be able to multiply and divide two values

--- src/main.rs
+++ src/main.rs
@@ -4,80 +4,94 @@ mod operations;
 use operations::Operation;
 use operations::Addition;
 use operations::Subtraction;
+use operations::Multiplication;
+use operations::Division;
+
+
+// Function to display the choices
+fn display_choices() {
+    println!("Enter your choice: ");
+    println!("1. Sum of two values");
+    println!("2. Subtraction of two values");
+    println!("3. Multiplication of two values");
+    println!("4. Division of two values");
+    println!("5. Quit");
+}
+
+// Function to read and parse the choice input
+fn get_choice() -> i32 {
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice)
+        .expect("Failed to read line");
+
+    match choice.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input, try again.");
+            return get_choice();
+        },
+    }
+}
+
+// Function to read and parse the first value input
+fn get_first_value() -> i32 {
+    println!("Enter first value: ");
+    let mut first = String::new();
+    io::stdin().read_line(&mut first)
+        .expect("Failed to read line");
+    match first.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input, try again.");
+            return get_first_value();
+        },
+    }
+}
+
+// Function to read and parse the second value input
+fn get_second_value() -> i32 {
+    println!("Enter second value: ");
+    let mut second = String::new();
+    io::stdin().read_line(&mut second)
+        .expect("Failed to read line");
+    match second.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input, try again.");
+            return get_second_value();
+        },
+    }
+}
 
 fn main() {
     println!("Welcome to the Rust calculator!");
     loop {
         let addition = Addition;
         let subtraction = Subtraction;
+        let multiplication = Multiplication;
+        let division = Division;
+        
         
         fn perform_operation<T: Operation>(operation: T, a: i32, b: i32) -> i32 {
             operation.calculate(a, b)
         }
-        // APPLYING LSP (LISKOV SUBSTITUTION)
-        pub struct Multiplication;
-        
-        impl Operation for Multiplication {
-        fn calculate(&self, a: i32, b: i32) -> i32 {
-            a * b
-        }
-        }
-        let multiplication = Multiplication;
-        
 
-        println!("Enter your choice: ");
-        println!("1. Sum of two values");
-        println!("2. Subtraction of two values");
-        println!("3. Multiplication of two values");
-        println!("4. Quit");
-
-        let mut choice = String::new();
-        io::stdin().read_line(&mut choice)
-            .expect("Failed to read line");
-
-        let choice: i32 = match choice.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Invalid input, try again.");
-                continue;
-            },
-        };
-
-        if choice == 4 {
+        display_choices();
+        let choice = get_choice();
+        if choice == 5 {
             break;
         }
-
-        println!("Enter first value: ");
-        let mut first = String::new();
-        io::stdin().read_line(&mut first)
-            .expect("Failed to read line");
-        let first: i32 = match first.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Invalid input, try again.");
-                continue;
-            },
-        };
-
-        println!("Enter second value: ");
-        let mut second = String::new();
-        io::stdin().read_line(&mut second)
-            .expect("Failed to read line");
-        let second: i32 = match second.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Invalid input, try again.");
-                continue;
-            },
-        };
-
+        let first = get_first_value();
+        let second = get_second_value();
 
         if choice == 1 {
             println!("The sum of {} and {} is: {}", first, second, perform_operation(addition, first, second));
         } else if choice == 2 {
             println!("The subtraction of {} and {} is: {}", first, second, perform_operation(subtraction, first, second));
         } else if choice == 3 {
-            println!("The subtraction of {} and {} is: {}", first, second, perform_operation(multiplication, first, second));
+            println!("The multiplication of {} and {} is: {}", first, second, perform_operation(multiplication, first, second));
+        } else if choice == 4 {
+            println!("The division of {} and {} is: {}", first, second, perform_operation(division, first, second));
         }
     }
 }
--- src/operations.rs
+++ src/operations.rs
@@ -2,8 +2,11 @@ pub trait Operation {
     fn calculate(&self, a: i32, b: i32) -> i32;
 }
 
+
 pub struct Addition;
 pub struct Subtraction;
+pub struct Multiplication;
+pub struct Division;
 
 
 impl Operation for Addition {
@@ -15,5 +18,17 @@ impl Operation for Addition {
 impl Operation for Subtraction {
     fn calculate(&self, a: i32, b: i32) -> i32 {
         a - b
+    }
+}
+
+impl Operation for Multiplication {
+    fn calculate(&self, a: i32, b: i32) -> i32 {
+        a * b
+    }
+}
+
+impl Operation for Division {
+    fn calculate(&self, a: i32, b: i32) -> i32 {
+        a / b
     }
 }
